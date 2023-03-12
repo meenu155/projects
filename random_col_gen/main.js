548fb05fab8cb5e763a6b5136ac119aca11455f4
meenu155 <112775342+meenu155@users.noreply.github.com>
2023-03-12 23:18:04 +0530

    Add files via upload

---
+++ random_col_gen/main.js
@@ -0,0 +1,12 @@
+const getcolor =()=>
+{
+const randomNumber=Math.floor(Math.random()* 16777215);
+const randomCode='#'+randomNumber.toString(16);
+//console.log(randomNumber,randomCode);
+document.body.style.backgroundColor=randomCode;
+document.getElementById("col-code").innerText=randomCode;
+
+}
+document.getElementById("btn").addEventListener("click",getcolor)
+getcolor()
+
